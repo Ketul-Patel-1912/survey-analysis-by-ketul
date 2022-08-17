8c2c9bbb10d96934a70c4da9853c3f1498e4ee10
Ketul <103981761+Ketul-Patel-1912@users.noreply.github.com>
2022-08-18 00:01:22 +0530

    Update app.py
    Imported openpyxl file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 from PIL import Image
+Import openpyxl
 
 st.set_page_config(page_title='Survey Results')
 st.header('Survey Results 2021')
