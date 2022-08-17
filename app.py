fb13a8c092b0e5167dd4dd8dd2ab18d7228382b1
hardik <hardikp211294@gmail.com>
2022-08-17 21:02:11 +0530

    new

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import streamlit as st
 import plotly.express as px
 from PIL import Image
 
-
-
 st.set_page_config(page_title='Survey Results')
 st.header('Survey Results 2021')
 st.subheader('Ketul Here!! Created first web APPLICATION')
